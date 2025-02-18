77f7ea964f1a4ce0ea2237ddc98a869a0f063429
LluisJV <lluisjv4@gmail.com>
2025-02-18 18:49:14 +0100

    app.use(express.static("public"));

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import express from "express"; // Correct import for ES modules
 const app = express();
 const port = 80;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+//pone los archivos estaticos en la carpeta public
+//para acceder a index.html -> http://localhost/index.html
+app.use(express.static("public"));
 
 app.listen(port, function () {
-  console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port: " + port);
 });
